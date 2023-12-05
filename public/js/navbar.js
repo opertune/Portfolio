6553e27e2c7bc021ffd401335aacc0a22aff2d47
Romain <r.baucheron@gmail.com>
2023-12-05 14:26:41 +0100

    Navbar + About content
    Navigation bar and About content (responsive)

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,13 +1,45 @@
-const checkbox = document.getElementById("hamburger-checkbox")
-// const navbar = document.getElementsByClassName("navbar")[0]
-checkbox.addEventListener("click", function () {
+const checkbox = document.getElementById('hamburger-checkbox')
+const navbar = document.querySelectorAll('.collapse')
+
+// Hide/Show navbar when user click on hamburger menu
+checkbox.addEventListener('click', function () {
     if (checkbox.checked) {
-        // navbar.style.transform = "translateY(0%)"
+        navbar.forEach(function (ele) {
+            ele.classList.add('show')
+            ele.classList.remove('hide')
+        })
+
     } else {
-        // navbar.style.transform = "translateY(-150%)"
+        navbar.forEach(function (ele) {
+            ele.classList.add('hide')
+            ele.classList.remove('show')
+        })
     }
 })
 
-document.getElementById("mailto").addEventListener("click", function () {
-    window.open('mailto:address@mail.com?subject=sub&body=this is body')
-})
+
+// Hide navbar when user click on list items | Show bottom border when user click on list items (>800px)
+const clickList = document.querySelectorAll('.nav-list')
+var oldClick = null
+clickList.forEach(function (element) {
+    element.addEventListener('click', function () {
+        // if screen width is less than 800px show/hide dropdown menu else show active border on the clicked element
+        if (window.innerWidth <= 800) {
+            navbar.forEach(function (ele) {
+                ele.classList.add('hide')
+                ele.classList.remove('show')
+            })
+            checkbox.checked = false
+        } else {
+            // Added bottom border exept for contacts
+            if (!element.classList.contains('click-contact')) {
+                element.classList.add('active')
+            }
+            // remove bottom border on change
+            if (oldClick !== null && oldClick !== element) {
+                oldClick.classList.remove('active')
+            }
+            oldClick = element
+        }
+    })
+});
---
+++ public/js/subpage.js
@@ -0,0 +1,7 @@
+const aboutText = document.querySelectorAll('.about-text')[0]
+const aboutPicture = document.querySelectorAll('.about-picture')[0]
+
+document.addEventListener('DOMContentLoaded', function () {
+    aboutText.classList.add('about-show')
+    aboutPicture.classList.add('about-show')
+})
